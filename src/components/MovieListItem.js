e88396c5176726b73fd7af19ac88300695f0cef5
sjvelandia <sjvelandia@gmail.com>
2018-05-23 18:31:06 -0700

    MovieListItem

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,23 +1,11 @@
-import React, {Component} from 'react';
-import MovieListItem from './MovieListItem';
+import React from 'react';
 
-class MoviesList extends Component {
+const movieListItem = (props) => (
+    <div className="movie-card">
+        <h3>{props.movie.title}</h3>
+        <div>{props.movie.release_year}</div>
+        <div className="movie-location">{props.movie.locations}</div>
+    </div>
+);
 
-    render() {
-        let movies = this.props.movies.map(
-            movie => {
-                return (
-                    <MovieListItem key={movie.id} movie={movie}/>
-                )
-            }
-        );
-
-        return (
-            <section className="movies">
-                {movies}
-            </section>
-        );
-    }
-}
-
-export default MoviesList;
+export default movieListItem;
